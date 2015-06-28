bbbbaa189d14c7a5fe3d47be42721c63140390b7
makarov <lesha3005@gmail.com>
2015-06-29 02:51:02 +0300

    rate wrapper

---
+++ app/src/main/java/apps/makarov/com/whereismycurrency/models/presenters/RateWrapper.java
@@ -0,0 +1,16 @@
+package apps.makarov.com.whereismycurrency.models.presenters;
+
+import apps.makarov.com.whereismycurrency.models.Rate;
+
+/**
+ * Created by makarov on 29/06/15.
+ */
+
+public class RateWrapper {
+
+    private final Rate mRate;
+
+    public RateWrapper(Rate rate){
+        mRate = rate;
+    }
+}
