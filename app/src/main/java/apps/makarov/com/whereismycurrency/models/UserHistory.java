d98ba96388a1b33a077e3c54a3b79c24d5df2fba
makarov <lesha3005@gmail.com>
2015-06-28 03:02:48 +0300

    added history model

---
+++ app/src/main/java/apps/makarov/com/whereismycurrency/models/UserHistory.java
@@ -0,0 +1,43 @@
+package apps.makarov.com.whereismycurrency.models;
+
+import java.util.Date;
+
+import io.realm.RealmObject;
+import io.realm.annotations.PrimaryKey;
+
+/**
+ * Created by makarov on 28/06/15.
+ */
+public class UserHistory extends RealmObject {
+
+    private Rate rate;
+    private double value;
+    @PrimaryKey
+    private Date date;
+
+    public Rate getRate() {
+        return rate;
+    }
+
+    public void setRate(Rate rate) {
+        this.rate = rate;
+    }
+
+    public double getValue() {
+        return value;
+    }
+
+    public void setValue(double value) {
+        this.value = value;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+
+}
