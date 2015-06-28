9863ce1d54aacad355a5b5977409fd5be271cb10
makarov <lesha3005@gmail.com>
2015-06-29 01:00:51 +0300

    created parser for HistoryRate

---
+++ app/src/main/java/apps/makarov/com/whereismycurrency/interpreter/HistoryRateInterpreter.java
@@ -0,0 +1,14 @@
+package apps.makarov.com.whereismycurrency.interpreter;
+
+import apps.makarov.com.whereismycurrency.models.Rate;
+
+/**
+ * Created by makarov on 29/06/15.
+ */
+public class HistoryRateInterpreter implements Interpreter<Rate> {
+
+    @Override
+    public Rate parse() {
+        return null;
+    }
+}
